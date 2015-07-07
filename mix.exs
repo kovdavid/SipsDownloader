c0345eac5bd215bc0f81e6a7a1499cffe8059af1
Davs <kovdavid@gmail.com>
2015-07-07 22:54:19 +0200

    Update mix.exs and LICENSE.txt

--- mix.exs
+++ mix.exs
@@ -21,7 +21,7 @@ defmodule SipsDownloader.Mixfile do
 
   defp package do
     [
-      contributors: ["David Kovacs"],
+      contributors: ["Dávid Kovács"],
       files: ["bin", "config", "lib", "mix.exs", "README*", "LICENSE*"],
       licenses: ["MIT"],
       links: %{"GitHub" => "https://github.com/DavsX/SipsDownloader"}

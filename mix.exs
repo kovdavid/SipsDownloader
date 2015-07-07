56a5019957bd7ba31868749bfa2f0d84ae2e6d4e
Davs <kovdavid@gmail.com>
2015-07-07 22:23:39 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -3,29 +3,34 @@ defmodule SipsDownloader.Mixfile do
 
   def project do
     [app: :sips_downloader,
-     version: "0.0.1",
+     version: "0.1.0",
+     name: "SipsDownloader",
+     source_url: "https://github.com/DavsX/SipsDownloader",
      elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
+     description: description,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
+  defp description do
+    """
+    Automatically download new ElixirSips episodes
+    """
+  end
+
+  defp package do
+    [
+      contributors: ["David Kovacs"],
+      files: ["bin", "config", "lib", "mix.exs", "README*", "LICENSE*"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/DavsX/SipsDownloader"}
+    ]
+  end
+
   def application do
     [applications: [:httpoison]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:httpoison, "~> 0.7"},
      {:sweet_xml, "~> 0.2"},

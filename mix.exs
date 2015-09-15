0467730e7ecc36e00b2016edc20b31e22bdbde09
Davs <kovdavid@gmail.com>
2015-09-15 22:44:17 +0200

    Bump version to 0.2.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SipsDownloader.Mixfile do
 
   def project do
     [app: :sips_downloader,
-     version: "0.1.0",
+     version: "0.2.0",
      name: "SipsDownloader",
      source_url: "https://github.com/DavsX/SipsDownloader",
      elixir: "~> 1.0",
